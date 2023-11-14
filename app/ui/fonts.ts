6e8a6358f7a2c1b9752b5d486cb1138d83588a2c
yajium <yajiuma720@gmail.com>
2023-11-14 21:40:10 +0900

    Add new pages and components to dashboard and update global styles

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Lusitana, Noto_Sans_JP } from 'next/font/google';
+
+export const notoSansJp = Noto_Sans_JP({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
